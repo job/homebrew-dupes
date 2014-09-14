23b51d5f1ac6f03710070ae880ec788e372c3398
Job Snijders <job@instituut.net>
2014-09-14 13:03:16 +0200

    Enable IDN support in whois

--- whois.rb
+++ whois.rb
@@ -5,10 +5,11 @@ class Whois < Formula
   url "http://ftp.us.debian.org/debian/pool/main/w/whois/whois_5.2.0.tar.xz"
   sha1 "1678a34b7b055750ba6f10d93eaa3b6c63993645"
 
+  depends_on "libidn"
   depends_on "xz" => :build
 
   def install
-    system "make HAVE_ICONV=1 whois_LDADD+=-liconv whois"
+    system "make HAVE_ICONV=1 whois_LDADD+='-liconv -lidn' HAVE_LIBIDN=1 whois"
     bin.install "whois"
     man1.install "whois.1"
   end
